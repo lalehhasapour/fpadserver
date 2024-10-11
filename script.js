10f4840068c6bb6f9ecef403e321c25848d9fd5b
lalehhasapour <lalehhasanpour6031@gmail.com>
2024-10-12 00:11:39 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,111 @@
-window.addEventListener('scroll', function() {
-    let features = document.querySelector('.features h2');
-    let mainFeatures = document.querySelectorAll('.main-features h3, .main-features p');
-    let footer = document.querySelector('footer h2, footer p');
+class TextGlitch {
+	constructor( root ) {
+		this._root = root;
+		this._elClips = root.querySelectorAll( ".TextGlitch-clip" );
+		this._elWords = root.querySelectorAll( ".TextGlitch-word" );
+		this._frame = this._frame.bind( this );
+		this._unglitch = this._unglitch.bind( this );
+		this._frameId = null;
+		this._text = "";
+		this._textAlt = [];
+		Object.seal( this );
 
-    // Trigger animation for features section
-    if (window.scrollY > 100) {
-        features.style.opacity = '1';
-        features.style.transform = 'translateY(0)';
-    }
+		this.setTexts( [
+			"hello friend!",
+			"HELLO FRIEND?",
+			"µ37(0 [R132q?",
+			"µ31)* {&13Nb?",
+			"#+:|* {&><@$?",
+		] );
+		
+		// this.setTexts( [
+		// 	"hello world !",
+		// 	"HELLO WORLD ?",
+		// 	"µ3770 3027q ?",
+		// 	"µ311p MQ51b ?",
+		// ] );
+	}
 
-    // Trigger animation for main features section
-    if (window.scrollY > 400) {
-        mainFeatures.forEach(item => {
-            item.style.opacity = '1';
-            item.style.transform = 'translateY(0)';
-        });
-    }
+	on() {
+		if ( !this._frameId ) {
+			this._frame();
+		}
+	}
+	off() {
+		clearTimeout( this._frameId );
+		this._frameId = null;
+		this._unglitch();
+	}
+	setTexts( [ text, ...alt ] ) {
+		this._text = text;
+		this._textAlt = alt;
+	}
 
-    // Trigger animation for footer
-    if (window.scrollY > 800) {
-        footer.style.opacity = '1';
-        footer.style.transform = 'translateY(0)';
-    }
-});
+	// private:
+	// .....................................................................
+	_frame() {
+		this._glitch();
+		setTimeout( this._unglitch, 50 + Math.random() * 200 );
+		this._frameId = setTimeout( this._frame, 250 + Math.random() * 500 );
+	}
+	_glitch() {
+		this._addClipCSS();
+		this._textContent( this._randText() );
+		this._root.classList.add( "TextGlitch-blended" );
+	}
+	_unglitch() {
+		this._removeClipCSS();
+		this._textContent( this._text );
+		this._root.classList.remove( "TextGlitch-blended" );
+	}
+	_textContent( txt ) {
+		this._elWords.forEach( el => el.textContent = txt );
+	}
+
+	// CSS clip-path, to cut the letters like an overflow:hidden
+	// .....................................................................
+	_addClipCSS() {
+		const clips = this._elClips,
+			clip1 = this._randDouble( .1 ),
+			clip2 = this._randDouble( .1 );
+
+		clips[ 0 ].style.transform = `translate(${ this._randDouble( .3 ) }em, .02em)`;
+		clips[ 2 ].style.transform = `translate(${ this._randDouble( .3 ) }em, -.02em)`;
+		clips[ 0 ].style.clipPath = `inset( 0 0 ${ .6 + clip1 }em 0 )`;
+		clips[ 1 ].style.clipPath = `inset( ${ .4 - clip1 }em 0 ${ .3 - clip2 }em 0 )`;
+		clips[ 2 ].style.clipPath = `inset( ${ .7 + clip2 }em 0 0 0 )`;
+	}
+	_removeClipCSS() {
+		this._elClips.forEach( el => {
+			el.style.clipPath =
+			el.style.transform = "";
+		} );
+	}
+
+	// Switch some chars randomly
+	// .....................................................................
+	_randText() {
+		const txt = Array.from( this._text );
+
+		for ( let i = 0; i < 12; ++i ) {
+			const ind = this._randInt( this._text.length );
+
+			txt[ ind ] = this._textAlt[ this._randInt( this._textAlt.length ) ][ ind ];
+		}
+		return txt.join( "" );
+	}
+
+	// rand utils
+	// .....................................................................
+	_randDouble( d ) {
+		return Math.random() * d - d / 2;
+	}
+	_randInt( n ) {
+		return Math.random() * n | 0;
+	}
+}
+
+const elTitle = document.querySelector( "#title" );
+const glitch = new TextGlitch( elTitle );
+
+glitch.on();
